a140560a45dc4afa64c055cb710f8cc12f5f2b51
Paulina503 <pauyolandacervantes@gmail.com>
2023-10-03 19:35:53 -0700

    main

--- gestion-usuarios/scripr.js
+++ gestion-usuarios/scripr.js
@@ -1,3 +1,20 @@
 const nameInput = document.getElementById("name")
 const emailInput = document.getElementById("email")
-const userList = 
+const userList = document.getElementById("user-list")
+const crudForm = document.getElementById("crud-form")
+const apiKey = //"b3a71d00b55d4caf87e3879fd8df4e03" //AQUI VA EL API KEY
+const apiUrl = `https://crudcrud.com/api/${apiKey}/users`
+
+function loadUsers() {
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data =>{ //arreglo de objetos literales con la info. de los usuarios
+        userList.innerHTML = ''
+        data.forEach(user => {
+            const li = document.createElement('li')
+            li.innerHTML = `
+                <strong> ${user.name} </strong>
+            `
+        });
+    })
+}
